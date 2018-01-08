b09dc18ddbdcf8dbdc7c8d55154ef51b731d3ca2
loki2302 <loki2302@loki2302.me>
2018-01-08 00:13:07 -0500

    It's now an Angular app

--- electron-builder-experiment/src/renderer/index.ts
+++ electron-builder-experiment/src/renderer/index.ts
@@ -1,1 +1,40 @@
-document.querySelector('#app').innerHTML = 'hello world';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {Component, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+
+@Component({
+    selector: '#app',
+    template: `
+        <h1>hello world</h1>
+        <p>counter: {{count}}</p>
+        <button type="button" (click)="increment()">Click me!</button>
+    `
+})
+export class AppComponent {
+    count: number = 0;
+
+    increment() {
+        ++this.count;
+    }
+}
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {
+}
+
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.log(err));
